71e829ef06886d63a8f1d001e1ba3e3cf2a5cbaa
Yuukin256 <Yuukin256@gmail.com>
2020-05-04 00:45:18 +0900

    調整

--- jawp-sysop/webpack.config.js
+++ jawp-sysop/webpack.config.js
@@ -1,35 +1,44 @@
 const path = require('path');
+const CopyFilePlugin = require('copy-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
 module.exports = {
-  // モジュールバンドルを行う起点となるファイルの指定
-  // 指定できる値としては、ファイル名の文字列や、それを並べた配列やオブジェクト
-  // 下記はオブジェクトとして指定した例
   entry: {
     bundle: './src/index.ts',
   },
   output: {
-    // モジュールバンドルを行った結果を出力する場所やファイル名の指定
-    // "__dirname"はこのファイルが存在するディレクトリを表すnode.jsで定義済みの定数
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js', // [name]はentryで記述した名前(この例ではbundle）が入る
+    filename: '[name].js',
   },
-  // モジュールとして扱いたいファイルの拡張子を指定する
-  // 例えば「import Foo from './foo'」という記述に対して"foo.ts"という名前のファイルをモジュールとして探す
-  // デフォルトは['.js', '.json']
   resolve: {
     extensions: ['.ts', '.js', '.json'],
   },
   devServer: {
-    // webpack-dev-serverの公開フォルダ
     contentBase: path.join(__dirname, 'dist'),
   },
-  // モジュールに適用するルールの設定（ここではローダーの設定を行う事が多い）
   module: {
     rules: [
       {
-        // 拡張子が.tsで終わるファイルに対して、TypeScriptコンパイラを適用する
         test: /\.ts$/,
         loader: 'ts-loader',
       },
     ],
   },
+  plugins: [
+    new CopyFilePlugin(
+      [
+        {
+          context: 'src',
+          from: '**/*.html',
+          to: path.resolve(__dirname, 'dist'),
+        },
+        {
+          context: 'src',
+          from: '**/*.css',
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+      { copyUnmodified: true }
+    ),
+    new WriteFilePlugin(),
+  ],
 };
