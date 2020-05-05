4e109a94bfc2e00764f3710832628ee4bea94d2b
Yuukin256 <Yuukin256@gmail.com>
2020-05-05 21:00:12 +0900

    見た目の調整

--- site/src/index.ts
+++ site/src/index.ts
@@ -93,124 +93,154 @@ const init = () => {
     return userData;
   };
 
-  const sysopData = makeData(json.sysops, [3, 'month']);
-  const rollbackerData = makeData(json.rollbackers, [1, 'year']);
-  const eliminatorData = makeData(json.eliminators, [1, 'year']);
-  const interfaceAdminData = makeData(json.interfaceAdmins, [6, 'month']);
-
+  // 日時フォーマットパターン
   const datePattern = 'YYYY/MM/DD HH:mm:ss';
 
-  // 最終更新/ページ描画
-  $('#lastUpdate')
-    .empty()
-    .append(
-      $('<ul></ul>').append(
-        $('<li>データ最終更新: ' + dayjs(json.lastUpdate).format(datePattern) + '</li>'),
-        $('<li>ページ描画: ' + dayjs().format(datePattern) + '</li>')
-      )
-    );
-
-  const makeTable = (table: JQuery<HTMLElement>, usersData: UserInfo[]): JQuery<HTMLElement> => {
-    // 表ヘッダー
-    (() => {
-      const tr = $('<tr></tr>');
-      const headers = ['項番', '利用者名', '最新編集 (差分)', '最新ログ (操作)', '最新編集/ログ', '自動退任日時'];
-      headers.forEach((value) => {
-        const th = $(`<th>${value}</th>`);
-        if (value === '最新編集 (差分)' || value === '最新ログ (操作)') {
-          th.addClass('can-hide');
-        }
-        tr.append(th);
-      });
-      table.append(tr);
-    })();
-
+  // 表作成関数
+  const makeTable = (
+    table: JQuery<HTMLElement>,
+    usersData: UserInfo[],
+    rmPeriodHeader: string = '自動退任日時'
+  ): JQuery<HTMLElement> => {
     // チェックマーク
     const okMark = () => $('<span>✓</span>').addClass('ok-mark');
 
     // バツマーク
     const ngMark = () => $('<span>✘</span>').addClass('ng-mark');
 
-    usersData.forEach((v, index) => {
+    // ウィキペディア日本語版 URLエンドポイント
+    const wpEndpoint = 'https://ja.wikipedia.org/w/index.php';
+
+    // 表ヘッダー
+    (() => {
       const tr = $('<tr></tr>');
+      const headers = ['項番', '利用者', '最新編集 (差分)', '最新ログ (操作)', '最新編集/ログ', rmPeriodHeader];
+      headers.forEach((value) => {
+        const th = $(`<th>${value}</th>`);
+        if (value === '最新編集 (差分)' || value === '最新ログ (操作)') {
+          th.addClass('can-hide');
+        }
+        th.appendTo(tr);
+      });
+      tr.appendTo(table);
+    })();
 
-      // 項番
-      tr.append($(`<td>${(index + 1).toString()}</td>`).addClass('index'));
+    // 表データ
+    usersData.forEach((v, index) => {
+      $('<tr></tr>')
+        .append(
+          // 項番
+          $(`<td>${(index + 1).toString()}</td>`).addClass('index'),
 
-      // 利用者名
-      tr.append($(`<td>${v.name}</td>`).addClass('username'));
+          // 利用者
+          (() => {
+            const name = $(`<a>${v.name}</a>`).addClass('wp-link').prop('href', `${wpEndpoint}?title=User:${v.name}`);
+            const links = $('<span></span>')
+              .addClass(['wp-link', 'small'])
+              .append(
+                '(',
+                $('<a>投稿記録</a>').prop('href', `${wpEndpoint}?title=Special:Contributions/${v.name}`),
+                ' / ',
+                $('<a>ログ</a>').prop('href', `${wpEndpoint}?title=Special:Log&user=${v.name}`),
+                ')'
+              );
+            return $(`<td></td>`).addClass('user').append(name, links);
+          })(),
 
-      // 最新編集 (差分)
-      (() => {
-        const mark = v.lastEditOk ? okMark() : ngMark();
-        const timestamp = $(`<span>${v.lastEditTimestamp.format(datePattern)}</span>`).addClass('timestamp');
-        const link = $('<a>(差分)</a>').prop('href', 'https://ja.wikipedia.org/w/index.php?diff=' + v.lastEditId);
-        tr.append($('<td></td>').addClass('can-hide').append(mark, timestamp, link));
-      })();
+          // 最新編集 (差分)
+          (() => {
+            const mark = v.lastEditOk ? okMark() : ngMark();
+            const timestamp = $(`<span>${v.lastEditTimestamp.format(datePattern)}</span>`).addClass('timestamp');
+            const link = $('<span></span>')
+              .addClass(['wp-link', 'small'])
+              .append('(', $('<a>差分</a>').prop('href', `${wpEndpoint}?diff=${v.lastEditId}`), ')');
+            return $('<td></td>').addClass('can-hide').append(mark, timestamp, link);
+          })(),
 
-      // 最新ログ (操作)
-      (() => {
-        const mark = v.lastEventOk ? okMark() : ngMark();
-        const timestamp = $(`<span>${v.lastEventTimestamp.format(datePattern)}</span>`).addClass('timestamp');
-        const link = $('<a>(操作)</a>').prop(
-          'href',
-          'https://ja.wikipedia.org/w/index.php?title=Special:Log&logid=' + v.lastEventId
-        );
-        tr.append($('<td></td>').addClass('can-hide').append(mark, timestamp, link));
-      })();
+          // 最新ログ (操作)
+          (() => {
+            const mark = v.lastEventOk ? okMark() : ngMark();
+            const timestamp = $(`<span>${v.lastEventTimestamp.format(datePattern)}</span>`).addClass('timestamp');
+            const link = $('<span></span>')
+              .addClass(['wp-link', 'small'])
+              .append(
+                '(',
+                $('<a>操作</a>').prop('href', `${wpEndpoint}?title=Special:Log&logid=${v.lastEventId}`),
+                ')'
+              );
+            return $('<td></td>').addClass('can-hide').append(mark, timestamp, link);
+          })(),
 
-      // 最新編集/ログ
-      (() => {
-        const mark = v.lastActionOk ? okMark() : ngMark();
-        const timestamp = $(`<span>${v.lastActionTimestamp.format(datePattern)}</span>`).addClass('timestamp');
-        const link = $(`<a>${v.lastActionType === 'edit' ? '(編集)' : '(ログ)'}</a>`).prop(
-          'href',
-          'https://ja.wikipedia.org/w/index.php?' +
-            (v.lastActionType === 'edit' ? 'diff=' : 'title=Special:Log&logid=') +
-            v.lastActionId
-        );
-        tr.append($('<td></td>').append(mark, timestamp, link));
-      })();
+          // 最新編集/ログ
+          (() => {
+            const mark = v.lastActionOk ? okMark() : ngMark();
+            const timestamp = $(`<span>${v.lastActionTimestamp.format(datePattern)}</span>`).addClass('timestamp');
+            const linkContent =
+              v.lastActionType === 'edit'
+                ? $('<a>編集</a>').prop('href', `${wpEndpoint}?diff=${v.lastEditId}`)
+                : $('<a>ログ</a>').prop('href', `${wpEndpoint}?title=Special:Log&logid=${v.lastEventId}`);
+            const link = $(`<span></span>`).addClass(['wp-link', 'small']).append('(', linkContent, ')');
+            return $('<td></td>').append(mark, timestamp, link);
+          })(),
 
-      // 権限除去/自動退任 日時
-      (() => {
-        const mark = v.rightOk ? okMark() : ngMark();
-        const timestamp = $(`<span>${v.estimatedRmRight.format(datePattern)}</span>`).addClass('timestamp');
-        tr.append($('<td></td>').append(mark, timestamp));
-      })();
-
-      // 行追加
-      table.append(tr);
+          // 権限除去/自動退任 日時
+          (() => {
+            const mark = v.rightOk ? okMark() : ngMark();
+            const timestamp = $(`<span>${v.estimatedRmRight.format(datePattern)}</span>`).addClass('timestamp');
+            return $('<td></td>').append(mark, timestamp);
+          })()
+        )
+        .appendTo(table);
     });
 
     return table;
   };
 
+  // 表を作成
+  // 管理者
   makeTable(
     $('#sysopTable').html(
       '<caption>管理者 <span class="small">(BC CU OS 兼任含む)</span><br /><span class="small">3か月で自動退任</span></caption>'
     ),
-    sysopData
+    makeData(json.sysops, [3, 'month']),
+    '最新より3か月'
   );
+  // 巻き戻し者
   makeTable(
     $('#rollbackerTable').html(
       '<caption>巻き戻し者<br /><span class="small">1年で会話ページ通知 その後1か月で権限除去提案</span></caption>'
     ),
-    rollbackerData
+    makeData(json.rollbackers, [1, 'year']),
+    '最新より1年'
   );
+  // 削除者
   makeTable(
     $('#eliminatorTable').html(
       '<caption>削除者<br /><span class="small">1年で会話ページ通知 その後1か月で権限除去提案</span></caption>'
     ),
-    eliminatorData
+    makeData(json.eliminators, [1, 'year']),
+    '最新より1年'
   );
+  // インターフェース管理者
   makeTable(
     $('#interfaceAdminTable').html(
       '<caption>インターフェース管理者<br /><span class="small">6か月で権限除去提案</span></caption>'
     ),
-    interfaceAdminData
+    makeData(json.interfaceAdmins, [6, 'month']),
+    '最新より6か月'
   );
 
+  // 最終更新/ページ描画
+  $('#lastUpdate')
+    .empty()
+    .append(
+      $('<ul></ul>').append(
+        $(`<li>データ最終更新: ${dayjs(json.lastUpdate).format(datePattern)}</li>`),
+        $(`<li>ページ描画: ${dayjs().format(datePattern)}</li>`)
+      )
+    );
+
+  // タイムゾーン切り替えスイッチの調整
   if (usingUTC) {
     $('#utcToggle').addClass('active');
     $('#localToggle').removeClass('active');
